033717dd46741c0f78d9e6997f806ce1c3dfa372
KeithCaVin <54549002+KeithCaVin@users.noreply.github.com>
2021-05-11 09:33:32 +0400

    fixed env and deploy

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import './App.css';
 import ShowData from './components/ShowData';
 import Home from './components/Home';
 import {BrowserRouter as Router, Route} from "react-router-dom"; 
+
 function App() {
-  
   return (
     <Router>
       <div>
 
-        <Route component={Home} path="/high-tech-deals" exact/>
+        <Route component={Home} path="/artefact-high-tech-deals" exact/>
         <Route component={ShowData} path="/result"/> 
-     
+    
           
       </div>
-    </Router>
+  </Router>
   );
-  
 }
 
 export default App;
