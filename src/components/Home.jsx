c9778daa0e8d828cf092d890f2342ed640b860f2
KeithCaVin <54549002+KeithCaVin@users.noreply.github.com>
2021-05-21 23:21:38 +0400

    added some logos in the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 
 import ShowData from './ShowData';
 import Logo from '../assets/logo.png';
+
+import AmazonLogo from '../assets/amazonlogo.png';
+import VirginMegastoreLogo from '../assets/virginlogo.png';
+import JumboLogo from '../assets/jumbologo.png';
 const Home = () =>{
     const [state, setState] = useState({
         searchWord:""
@@ -22,14 +26,20 @@ const Home = () =>{
                 <div className="page-logo">
                    <img class="htd-logo" src={Logo} alt="logo"/>
                 </div>
-             
+               
                 <form>
                     <input type="text" name="searchWord" placeholder="Search for a product" value={state.searchWord} onChange={handleChange} required />
                     <button class="search-btn" type="submit"> Search</button>
                 </form>
             </div>
 
-          
+            {queryString === '' && 
+                <div className="ecommerce-logos">
+                    <img className="amazon-logo" src={AmazonLogo} alt="Amazon Logo"/>
+                    <img className="virgin-logo" src={VirginMegastoreLogo} alt="Virgin Megastore Logo"/>
+                    <img className="jumbo-logo" src={JumboLogo} alt="Jumbo Logo"/>
+                </div>
+            }
                 
                 {keyword && 
                     <div className="searched-data">
